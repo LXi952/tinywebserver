8624da107c09ec8a36069a16da5de29c542b5ce5
LXi952 <63639016+LXi952@users.noreply.github.com>
2025-03-02 14:02:05 +0800

    Add files via upload

---
+++ common/common.h
@@ -0,0 +1,43 @@
+/*************************************************************************
+    > File Name: common.h
+    > Author: 
+    > Mail: 
+    > Created Time: Thu 20 Feb 2025 07:58:10 PM CST
+ ************************************************************************/
+
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <cassert>
+#include <fcntl.h>
+#include <string.h>
+#include <stdarg.h>
+#include <pthread.h>
+#include <time.h>
+#include <signal.h>
+#include <cstdio>
+#include <assert.h>
+
+#include <iostream>
+#include <string>
+#include <list>
+#include <exception>
+#include <map>
+
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/uio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <mysql/mysql.h>
+
+#endif
